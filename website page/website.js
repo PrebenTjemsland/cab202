6537151fa898b365b152ef2c0a0e7b77b1bd6464
benlynch96 <ben.lynch.96@gmail.com>
2018-05-23 19:31:20 +1000

    Maps
    Maps

--- website page/website.js
+++ website page/website.js
@@ -10,6 +10,18 @@
         });
       }
 	  
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map2'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+	  
         function codeAddress() {
             alert('ok');
         }
