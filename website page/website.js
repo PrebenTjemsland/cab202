4e184bce74004ec0885b0a1b5de9fb6123116b6a
benlynch96 <ben.lynch.96@gmail.com>
2018-05-25 17:01:44 +1000

    '

--- website page/website.js
+++ website page/website.js
@@ -11,18 +11,13 @@
       }
 	  
       function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var brisbane = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map2'), {
           zoom: 4,
-          center: uluru
+          center: brisbane
         });
         var marker = new google.maps.Marker({
-          position: uluru,
+          position: brisbane,
           map: map
         });
       }
-	  
-        function codeAddress() {
-            alert('ok');
-        }
-		test
