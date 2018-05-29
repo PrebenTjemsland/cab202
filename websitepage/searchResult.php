7c66226a8d90f97545bab9dfad9a71b4cd714b20
PrebenTjemsland <prebt16@student.uia.no>
2018-05-29 16:28:44 +1000

    map

--- websitepage/searchResult.php
+++ websitepage/searchResult.php
@@ -1,7 +1,8 @@
 <?php
     //Establish connection    
-    include("config.php");   
+    include("config.php"); 
 ?>
+
 
 <html>
 	<title>Search Results</title>
@@ -10,20 +11,21 @@
 		</head>
 		<body>
 		<!-- Makes the bar at the top with the logo and navigation -->	
-			<div class="center" itemscope itemtype="http://schema.org/WPHeader" itemprop="navigation">
-		<!-- Places and Positions the Logo in the nav bar -->
-				<div class="topnav" itemscope itemtype="http://schema.org/SiteNavigationElement" itemprop="navigation" about="header">
-					<img src="../Resources/bcc.jpg" alt="Brisbane City Council logo" height="70" width="340">
-					<a href="register.php">Register</a>
-					<a class="active" href="searchResult.php">Parks</a>
+			<div class="center">
+		<!-- Places and Positions the Logo in the nav bar -->	
+				<img src="../Resources/bcc.jpg" alt="Brisbane City Council logo" height="70" width="70">
+		Brisbane City Council Wifi Parks
+				<div class="topnav">
 					<a href="searchpage.php">Home</a>
+					<a class="active" href="searchResult.php">Parks</a>
+					<a href="register.php">Register</a>
 				</div>
 			</div>
 			<br>
 			<br>
 			<br>
-				<div class="center1" itemscope itemtype="http://schema.org/ItemPage" about="results"> 
-					<table class="Result">
+			<div class="center1"> 
+				<table class="Result">
 <?php
 try {
 if(isset($_REQUEST['submit'])&&("" != ($_POST['SuburbSelected']))){
@@ -38,7 +40,15 @@ if(isset($_REQUEST['submit'])&&("" != ($_POST['SuburbSelected']))){
 
      $result = $pdo->query(" SELECT WifiID, Adress, Suburb, Latitude, Longitude, max(Rating), ROUND(AVG(Rating),2) AS AverageRating
                              FROM WifiSpots LEFT JOIN Reviews on Re_WifiID = WifiID 
-                             WHERE WifiID like '%".$WifiID."%' GROUP BY WifiID");   
+                             WHERE WifiID like '%".$WifiID."%' GROUP BY WifiID");  
+                            $resultWifi = $result;
+                            $resultWifiID = $resultWifi->fetchAll();
+            echo '<script type="text/javascript">';
+            echo  "var WifiID =".json_encode($resultWifiID);  
+            echo '</script>';
+        $result = $pdo->query(" SELECT WifiID, Adress, Suburb, Latitude, Longitude, max(Rating), ROUND(AVG(Rating),2) AS AverageRating
+                             FROM WifiSpots LEFT JOIN Reviews on Re_WifiID = WifiID 
+                             WHERE WifiID like '%".$WifiID."%' GROUP BY WifiID");  
     }
     
         else if(isset($_REQUEST['submit'])&&("" != ($_POST['Rating']))){
@@ -60,8 +70,8 @@ echo "{$e->getMessage()}";
 }
 echo "<table>
 <tr><th>Name
-</th><th>Address
-</th><th>Suburb
+</th><th>Adress
+</th><th>Subburb
 </th><th>Rating
 </th><th>More info
 </th></tr>";
@@ -75,25 +85,41 @@ foreach ($result as $WifiSpots) {
         "</td><td>".'<a href="searchResult2.php?park='.$WifiSpots['WifiID'] . '">More info</a></td></tr>';
         "</td></tr>";
     }
+    try
+    { 
+    $sth = $pdo->query("SELECT wifiID, longitude FROM wifispots");
+    $youapp = $sth->fetchAll();
+    echo '<script type="text/javascript">';
+    echo  "var long1 =".json_encode( $youapp );  
+    echo '</script>';
+    $sth = $pdo->query("SELECT latitude FROM wifispots");
+     $youapp1 = $sth->fetchAll();
+    echo '<script type="text/javascript">';
+    echo  "var lat1 =".json_encode( $youapp1 );  
+    echo '</script>';
+  } catch (Exception $e) {
+    echo $e->getMessage();
+  }
  ?>
     </table>
-				</div>
+        </div>
     
-				<br>
-				<br>
-				<br>
+<br>
+<br>
+<br>
 				<br>
 				<br>
 <!-- creates a map with googles API -->
-				<div id="map_1" itemscope itemtype="http://schema.org/Map" itemprop="map" maptype="Selected Park">
+				<div id="map_1">
 					<script type="text/javascript" src="website1.js"></script>
 				</div>
-					<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3h8y5u1ZuB8YNgthTMPxmmi_EBiDKAeY&callback=initMap"></script>
-				<div class="spaceForm"></div> 
-				<div class="spaceForm"></div> 
-<!-- creates a Footer to stay at the bottom of the page-->
-				<div class="footer" itemscope itemtype="http://schema.org/WPFooter" itemprop="footer" about="Made by">
+				<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3h8y5u1ZuB8YNgthTMPxmmi_EBiDKAeY&callback=initMap"></script>
+					<div class="spaceForm"></div> 
+					<div class="spaceForm"></div> 
+	<!-- creates a Footer to stay at the bottom of the page-->
+				<div class="footer">
 				<p>Made by Benjamin Lynch and Preben Tjemsland</p>
 				</div>
+			
 		</body>
 </html>
