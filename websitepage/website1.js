a379dfe5181cdd119f9d7b06159051354ac10b26
PrebenTjemsland <prebt16@student.uia.no>
2018-05-29 16:26:13 +1000

    map

--- websitepage/website1.js
+++ websitepage/website1.js
@@ -1,14 +1,42 @@
-      function initMap() {
-        var latlong = {lat: -27.4698, lng: 153.0251};
-        var map = new google.maps.Map(document.getElementById('map_1'), {
-          zoom: 12,
-          center: latlong
-        });
-        var marker = new google.maps.Marker({
-          position: latlong,
-          map: map
-        });
+        function markers(map){
+        for (x = 0; x <  WifiID.length;  x++){   
+            for (i = 0; i < long1.length; i++){     
+                var lat2 = parseFloat(lat1[i][0]);
+                var long2 = parseFloat(long1[i][1]);
+            if (WifiID[x][0] == long1[i][0]){
+                var latlong1 = {lat: lat2 , lng: long2};
+                var marker = new google.maps.Marker({
+            position: latlong1,
+            map: map,
+            title: long1[i][0] 
+            });
+        }
+    }
+             
+ }
+        }
+      function initMap(){
+        for (x = 0; x <  WifiID.length;  x++){   
+            for (i = 0; i < long1.length; i++){     
+            var lat2 = parseFloat(lat1[i][0]);
+            var long2 = parseFloat(long1[i][1]);
+        if (WifiID[x][0] == long1[i][0]){
+            var latlong1 = {lat: lat2 , lng: long2};
+            }
+        }
+          initMap1(latlong1);
       }
+      }
+        function initMap1(latlong1) {
+        var map;
+        map = new google.maps.Map(document.getElementById('map_1'), {
+          zoom: 10,
+          center: latlong1
+        });  
+       markers(map);  
+      }
+
+
 
 	   var check = function() {
       if (document.getElementById('Password').value ==
