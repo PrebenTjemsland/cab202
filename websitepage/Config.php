899abbe0f11dda3c86d7fe60f571909cb9af233e
PrebenTjemsland <prebt16@student.uia.no>
2018-05-29 16:30:33 +1000

    sessions

--- websitepage/Config.php
+++ websitepage/Config.php
@@ -1,4 +1,5 @@
 <?php
     $pdo = new PDO('mysql:host=cab230.sef.qut.edu.au:3306 ;dbname=n10240047', 'n10240047', 'kristiansand');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    session_start();
 ?>
